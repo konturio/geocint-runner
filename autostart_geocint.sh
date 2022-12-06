51818220fb9b3741466f76ede70784b199b7e9ae
avalasiuk <avalasiuk@kontur.io>
2022-12-06 20:48:34 +0300

    Add and improve comments

--- autostart_geocint.sh
+++ autostart_geocint.sh
@@ -11,6 +11,7 @@ cleanup() {
   rm -f ~/$GENERAL_FOLDER/make.lock
 }
 
+# execute installations and send message with details if error returned
 sh ~/geocint-runner/runner-install.sh || echo 'runner-install.sh returned an error, check logs for more infornation' | python3 scripts/slack_message.py $SLACK_CHANNEL "$SLACK_BOT_NAME" $SLACK_BOT_EMOJI
 sh ~/geocint-openstreetmap/openstreetmap-install.sh || echo 'openstreetmap-install.sh returned an error, check logs for more infornation' | python3 scripts/slack_message.py $SLACK_CHANNEL "$SLACK_BOT_NAME" $SLACK_BOT_EMOJI
 sh ~/$PRIVATE_REPO_NAME/install.sh || echo 'install.sh returned an error, check logs for more infornation' | python3 scripts/slack_message.py $SLACK_CHANNEL "$SLACK_BOT_NAME" $SLACK_BOT_EMOJI
@@ -55,7 +56,7 @@ if [ "$UPDATE_PRIVATE" = "true" ]; then
   cd ~/$PRIVATE_REPO_NAME; git pull --rebase --autostash || { git stash && git pull && echo 'git rebase autostash $PRIVATE_REPO_NAME failed, stash and pull executed' | python3 scripts/slack_message.py $SLACK_CHANNEL "$SLACK_BOT_NAME" $SLACK_BOT_EMOJI; }
 fi
 
-# Remove from general folder all files and folders except data and db
+# Remove from general folder all files and folders except data, db and deploy
 find ~/$GENERAL_FOLDER/. -type d -not -name  "d*" -not -name '*.*' | xargs rm -rf
 find ~/$GENERAL_FOLDER/ -maxdepth 1 -type f -delete
 
@@ -65,6 +66,7 @@ mkdir -p ~/files_shouldnt_be_copy
 mv ~/geocint-openstreetmap/README.md ~/files_shouldnt_be_copy/osm_readme.md
 mv ~/$PRIVATE_REPO_NAME/README.md ~/files_shouldnt_be_copy/private_readme.md
 
+# use nohup to make cp return error when target file already exuists
 rm -f ~/nohup.out
 nohup cp -ia ~/geocint-runner/* ~/$GENERAL_FOLDER 2>>~/nohup.out &
 nohup cp -ia ~/geocint-openstreetmap/* ~/$GENERAL_FOLDER 2>>~/nohup.out &
@@ -75,10 +77,11 @@ mv ~/files_shouldnt_be_copy/osm_readme.md ~/geocint-openstreetmap/README.md
 mv ~/files_shouldnt_be_copy/private_readme.md ~/$PRIVATE_REPO_NAME/README.md
 rm -r ~/files_shouldnt_be_copy
 
+# count number of copy conflicts
 sed -i '/^nohup/d' ~/nohup.out
-
 nohup_length="$(cat ~/nohup.out | grep -v ^$ | wc -l)"
 
+# if number of copy conflicts more than 0  - exit and send message with details to slack channel
 if [ $nohup_length -eq 0 ]
 then
   echo "Copy from geocint-runner, geocint-openstreetmap and $PRIVATE_REPO_NAME to geocint folder completed successfully" | python3 ~/geocint-runner/scripts/slack_message.py $SLACK_CHANNEL "$SLACK_BOT_NAME" $SLACK_BOT_EMOJI
@@ -87,12 +90,14 @@ else
   exit 1
 fi
 
-# Compose makefiles to one pipeline
+# compose variables from configuration to clean target
 echo "clean: ## [FINAL] Cleans the worktree for next nightly run. Does not clean non-repeating targets.
 	if [ -f data/planet-is-broken ]; then rm -rf data/planet-latest.osm.pbf ; fi
 	rm -rf $RM_DIRECTORIES
 	profile_make_clean $TARGET_TO_CLEAN
 	$CLEAN_OPTIONALLY" >> ~/$GENERAL_FOLDER/Makefile
+	
+#add clean target to Makefile
 echo "include $OSM_MAKE_NAME $PRIVATE_MAKE_NAME" >> ~/$GENERAL_FOLDER/Makefile
 
 cd ~/$GENERAL_FOLDER
@@ -101,6 +106,7 @@ cd ~/$GENERAL_FOLDER
 sed -i "1s/.*/export PGDATABASE = $PGDATABASE/" ~/$GENERAL_FOLDER/Makefile
 sed -i "4s/.*/all\: $ALL_TARGETS \#\# final target/" ~/$GENERAL_FOLDER/Makefile
 
+# run clean target before pipeline running
 profile_make clean
 
 # Check name of current git branch
@@ -108,7 +114,9 @@ branch_runner="$(cd ~/geocint-runner; git rev-parse --abbrev-ref HEAD)"
 branch_osm="$(cd ~/geocint-openstreetmap; git rev-parse --abbrev-ref HEAD)"
 branch_private="$(cd ~/$PRIVATE_REPO_NAME; git rev-parse --abbrev-ref HEAD)"
 
+# send message with run information
 echo "Geocint server: current geocint-runner branch is $branch_runner, geocint-openstreetmap branch is $branch_osm, $PRIVATE_REPO_NAME branch is $branch_private. Running $RUN_TARGETS targets." | python3 scripts/slack_message.py $SLACK_CHANNEL "$SLACK_BOT_NAME" $SLACK_BOT_EMOJI
+# run pipeline
 profile_make -j -k $RUN_TARGETS
 make -k -q -n --debug=b $RUN_TARGETS 2>&1 | grep -v Trying | grep -v Rejecting | grep -v implicit | grep -v "Looking for" | grep -v "Successfully remade" | tail -n+10 | python3 scripts/slack_message.py $SLACK_CHANNEL "$SLACK_BOT_NAME" $SLACK_BOT_EMOJI
 
