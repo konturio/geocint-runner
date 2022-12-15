082311f698607b4b6d84d6c243ef193603717a90
avalasiuk <avalasiuk@kontur.io>
2022-12-15 14:00:36 +0300

    Rewrite mod setting instructions

--- scripts/set_modes.sh
+++ scripts/set_modes.sh
@@ -7,10 +7,12 @@ export PGDATABASE
 # user setup
 sudo usermod -aG adm $PGDATABASE
 sudo usermod -aG $PGDATABASE www-data
-sudo mkdir -p ~/{public_html,domlogs}
+sudo mkdir -p ~/public_html
+sudo mkdir -p ~/domlogs
 sudo chown root:$PGDATABASE ~/domlogs
 sudo chown $PGDATABASE:$PGDATABASE ~/public_html
-sudo chmod 0750 ~/{public_html,domlogs}
+sudo chmod 0750 ~/public_html
+sudo chmod 0750 ~/domlogs
 sudo su $PGDATABASE -c "echo 'export PATH=\$PATH:/usr/local/pgsql/bin' >> /home/$PGDATABASE/.bashrc"
 
 echo "modes setup finished"
