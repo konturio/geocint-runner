331806f13f3e4482f1c5f7372e1b354a5161ac9c
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2022-12-26 13:11:30 +0300

    13988 add curl in dependency installation script

--- runner-install.sh
+++ runner-install.sh
@@ -9,6 +9,7 @@ sudo apt-get install -y cmake
 sudo apt install -y unzip
 sudo apt-get -y install python3-boto3 python3-botocore # amazon.aws.aws_s3
 sudo apt-get install -y cron
+sudo apt-get install -y curl
 
 # python-is-python3 is a convenient way to set up a symlink for /usr/bin/python, pointing to /usr/bin/python3
 sudo apt install -y python-is-python3
