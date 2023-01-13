c1d97e7fc63fd73a414ec0c2f9026101ab1e968f
avalasiuk <avalasiuk@kontur.io>
2023-01-13 15:52:53 +0300

    Remove pandas installatio from runner-install

--- runner-install.sh
+++ runner-install.sh
@@ -4,7 +4,6 @@
 sudo apt install -y python3-pip graphviz gawk
 pip3 install slack slackclient
 pip3 install https://github.com/konturio/make-profiler/archive/master.zip
-pip3 install pandas
 sudo apt-get install -y cmake
 sudo apt install -y unzip
 sudo apt-get -y install python3-boto3 python3-botocore # amazon.aws.aws_s3
