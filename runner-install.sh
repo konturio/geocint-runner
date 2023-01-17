6a20775a1f5520a061546b417aee45abfd2ab219
avalasiuk <avalasiuk@kontur.io>
2023-01-17 18:17:51 +0300

    Rewrite installation guide

--- runner-install.sh
+++ runner-install.sh
@@ -3,7 +3,7 @@
 # Installing make-profiler
 sudo apt install -y python3-pip graphviz gawk
 pip3 install slack slackclient
-pip3 install https://github.com/konturio/make-profiler/archive/master.zip
+sudo pip3 install https://github.com/konturio/make-profiler/archive/master.zip
 sudo apt-get install -y cmake
 sudo apt install -y unzip
 sudo apt-get -y install python3-boto3 python3-botocore # amazon.aws.aws_s3
