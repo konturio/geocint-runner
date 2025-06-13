ecb681f9fdb6ea0d39c57fbfcc1b497a97509d7b
Darafei Praliaskouski <me@komzpa.net>
2025-06-14 01:07:18 +0400

    Fix flake8 issues and add lint workflow

--- scripts/merge_repos_and_check_duplicates.py
+++ scripts/merge_repos_and_check_duplicates.py
@@ -1,97 +1,118 @@
 #!/usr/bin/python3
 
+import collections
 import os
 import shutil
 import sys
-import collections
-
 from re import search
 
-if len(sys.argv) < 2:
+
+if len(sys.argv) < 4:
     print(
-        'usage: merge_repos_and_check_duplicates.py [geocint-runner] [geocint-openstreetmap] [your private repo]'
+        "usage: merge_repos_and_check_duplicates.py "
+        "[geocint-runner] [geocint-openstreetmap] [your private repo]"
     )
+    sys.exit(1)
 
 runner = sys.argv[1]
 openstreetmap = sys.argv[2]
 private = sys.argv[3]
-       
+
+
 def get_files_list(directory, ignore_list):
     dir_list = os.walk(directory)
-    dir_list = [x for x in dir_list if not search('/\.', x[0])]
-    
-    # initialize empty list to store files names
+    dir_list = [x for x in dir_list if not search(r"/\.", x[0])]
+
     file_list = []
-    for i in dir_list:
-        for n in i[2]:
-            file_list.append((str(i[0])+'/'+str(n)).replace('//', '/')) 
-            
-    # recursively remove ignored files         
-    for i in ignore_list:
-        file_list = [x for x in file_list if not x.split('/')[1] == i]   
-        
+    for folder, _, filenames in dir_list:
+        for name in filenames:
+            file_list.append((str(folder) + "/" + str(name)).replace("//", "/"))
+
+    for item in ignore_list:
+        file_list = [x for x in file_list if x.split("/")[1] != item]
+
     return file_list
 
+
 def get_config_variable(config_name, variable, delimiter):
-    config = open(config_name, 'r')
-    
-    # initialize empty list to store ignored files
     ignored_files = []
-    
-    for i in config:
-        if search(variable, i):
-            # transform row with ignored files into list of variables
-            ignored_files = (i.split('='))[1].strip('\n').strip('"').strip("'").split(delimiter)
-    
+    with open(config_name, "r") as config:
+        for line in config:
+            if search(variable, line):
+                ignored_files = (
+                    line.split("=")[1]
+                    .strip("\n")
+                    .strip('"')
+                    .strip("'")
+                    .split(delimiter)
+                )
+
     return ignored_files
 
+
 def find_duplicates(files):
-    removed_repo = ['/'.join(x.split('/')[1:]) for x in files]
-    
-    # return filename patterns that occur more than once 
-    duplicated_patterns = [x for x, count in collections.Counter(removed_repo).items() if count > 1]
-    
+    removed_repo = ["/".join(x.split("/")[1:]) for x in files]
+
+    duplicated_patterns = [
+        x for x, count in collections.Counter(removed_repo).items() if count > 1
+    ]
+
     duplicates = []
-    for n in duplicated_patterns:
-        for x in files:
-            if '/'.join(x.split('/')[1:]) == n:
-                duplicates.append(x)
-    
+    for pattern in duplicated_patterns:
+        for filename in files:
+            if "/".join(filename.split("/")[1:]) == pattern:
+                duplicates.append(filename)
+
     return duplicates
-    
+
+
 def copy_folder_structure(directory, general_folder):
     dir_list = os.walk(directory)
-    dir_list = [x[0] for x in dir_list if not search('/\.', x[0])]
-    
-    folder_list = [general_folder+'/'+'/'.join(x.split('/')[1:]) for x in dir_list]
-    
+    dir_list = [x[0] for x in dir_list if not search(r"/\.", x[0])]
+
+    folder_list = [
+        general_folder + "/" + "/".join(x.split("/")[1:])
+        for x in dir_list
+    ]
+
     for path in folder_list:
         os.makedirs(path, exist_ok=True)
-    
+
+
 def main():
-    ignore_list = get_config_variable(str(os.getenv('GEOCINT_WORK_DIRECTORY'))+'/config.inc.sh','ALLOW_DUPLICATE_FILES',',')
-    general_folder = 'geocint'
-    
-    # create list with non-ignored files from all repositories  
-    files = []    
+    ignore_list = get_config_variable(
+        str(os.getenv("GEOCINT_WORK_DIRECTORY")) + "/config.inc.sh",
+        "ALLOW_DUPLICATE_FILES",
+        ",",
+    )
+    general_folder = "geocint"
+
+    files = []
     files += get_files_list(runner, ignore_list)
     files += get_files_list(openstreetmap, ignore_list)
     files += get_files_list(private, ignore_list)
-    
-    duplicated_files = find_duplicates(files)    
-    
-    # check if duplicates exist
+
+    duplicated_files = find_duplicates(files)
+
     if len(duplicated_files) > 0:
-        sys.stdout.write(f'Skip start: duplicate files were found while copying files to a {general_folder} folder: '+ ',\n'.join(duplicated_files) + '\n')
+        sys.stdout.write(
+            "Skip start: duplicate files were found while copying files to a "
+            f"{general_folder} folder: " + ",\n".join(duplicated_files) + "\n"
+        )
     else:
-        # copy folder structure
         copy_folder_structure(runner, general_folder)
         copy_folder_structure(openstreetmap, general_folder)
         copy_folder_structure(private, general_folder)
-        # copy files
-        for x in files:
-            shutil.copyfile(x, general_folder+'/'+'/'.join(x.split('/')[1:]))
-        sys.stdout.write(f'Copy from {runner}, {openstreetmap} and {private} to {general_folder} folder completed successfully\n')    
- 
-if __name__ == '__main__':
-    main() 
+        for file_name in files:
+            shutil.copyfile(
+                file_name,
+                general_folder + "/" + "/".join(file_name.split("/")[1:]),
+            )
+        sys.stdout.write(
+            f"Copy from {runner}, {openstreetmap} and {private} to {general_folder} "
+            "folder completed successfully\n"
+        )
+
+
+if __name__ == "__main__":
+    main()
